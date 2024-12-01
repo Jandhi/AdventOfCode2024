411fb36b588cb58bc5fa757d8d443664d2e67d10
Jan D <jandohring@hotmail.com>
2024-12-01 00:09:44 -0500

    quick and dirty

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,38 @@
 fn main() {
-    println!("Hello, world!");
+    let file = include_str!("input.txt");
+
+    let mut vec1 = vec![];
+    let mut vec2 = vec![];
+
+    for line in file.trim().lines() {
+        if line.is_empty() {
+            continue;
+        }
+        let parts = line.split("   ").collect::<Vec<&str>>();
+
+        vec1.push(parts[0].parse::<i32>().unwrap());
+        vec2.push(parts[1].parse::<i32>().unwrap());
+    }
+
+    let mut vec1copy = vec1.clone();
+    let mut vec2copy = vec2.clone();
+
+    vec1.sort();
+    vec2.sort();
+
+    let mut diffs = 0;
+
+    for i in 0..vec1.len() {
+        diffs += i32::abs(vec1[i] - vec2[i]);
+    }
+
+    println!("{}", diffs);
+
+    let mut score = 0;
+
+    for i in 0..vec1copy.len() {
+        score += (vec1copy[i] * vec2copy.iter().filter(|num| **num == vec1copy[i]).count() as i32);
+    }
+
+    println!("{}", score);
 }
